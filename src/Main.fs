7da7911e259ca05d3959370e08c9aab2a4bdb921
Alan Lau <alan.lau@flarehr.com>
2024-10-03 12:35:21 +1000

    Init

---
+++ src/Main.fs
@@ -0,0 +1,13 @@
+﻿module Main
+
+open Fable.Core.JsInterop
+open Feliz
+open Browser.Dom
+
+importSideEffects "./index.css"
+
+[<ReactComponent(import="App", from="./app.tsx")>]
+let App () = React.imported()
+
+let root = ReactDOM.createRoot(document.getElementById "app")
+root.render(App())
